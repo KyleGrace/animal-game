1a34cde69fc10cf51953cfab9238806b460195b1
kylegrace <kgrace@g.hmc.edu>
2021-04-15 16:57:13 -0700

    web app

---
+++ web-app/main.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    celsius = request.args.get("celsius","")
+    if celsius:
+        fahrenheit = fahrenheit_from(celsius)
+    else:
+        fahrenheit = ""
+    return (
+        """<form action="" method="get">
+                <input type="text" name="celsius">
+                <input type="submit" value="Convert">
+              </form>"""
+        + "Fahrenheit: "
+        + fahrenheit
+    )
+
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
+
+# # Used to test locally
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=8080,debug=True)
