20b97ce0ea6e0cfbfdfdf7235b2e6d3b64eaa0fd
elissahou <ehou@g.hmc.edu>
2021-04-15 21:20:02 -0700

    Very basic implementation of game

--- web-app/main.py
+++ web-app/main.py
@@ -1,38 +1,69 @@
-from flask import Flask
-from flask import request, render_template
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
+votes = 0
+
+game_metric=[10,10]
+
+prompts = {
+    "kill all animals": (-10, -9),
+    "kill half of the animals": (-5, -4),
+    "kill all humans": (5, -10),
+    "require everyone to be vegan": (5, -5),
+}
+
+keys = prompts.keys()
+game_prompt = random.choice(list(keys))
+animal_change, human_change = prompts[game_prompt]
+del prompts[game_prompt]
+
 @app.route("/")
 def index():
-    return(
-        render_template("index.html")
-    )
+    return render_template("index.html", votes=votes, game_metric=game_metric,game_prompt=game_prompt)
 
-@app.route("/temp")
-def temp():
-    celsius = request.args.get("celsius","")
-    if celsius:
-        fahrenheit = fahrenheit_from(celsius)
-    else:
-        fahrenheit = ""
-    return (
-        """<form action="" method="get">
-                <input type="text" name="celsius">
-                <input type="submit" value="Convert">
-              </form>"""
-        + "Fahrenheit: "
-        + fahrenheit
-    )
+@app.route("/up", methods=["POST"])
+def upvote():
+    global votes
+    votes = votes + 1
+    return str(votes)
 
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return str(fahrenheit)
-    except ValueError:
-        return "invalid input"
+@app.route("/down", methods=["POST"])
+def downvote():
+    global votes
+    if votes >= 1:
+        votes = votes - 1
+    return str(votes)
+
+
+
+@app.route("/yes", methods=["POST"])
+def optionyes():
+    global game_metric
+    # global animal_metric
+    # global human_metric
+    global game_prompt
+    global prompts
+    global animal_change
+    global human_change
+
+    # animal_change, human_change = prompts[game_prompt]
+    game_metric[0] += animal_change
+    game_metric[1] += human_change
+    # animal_metric += animal_change
+    # human_metric += human_change
+    # del prompts[game_prompt]
+
+    game_prompt = random.choice(list(keys))
+    animal_change, human_change = prompts[game_prompt]
+    del prompts[game_prompt]
+
+    return str(game_metric)
+
+# @app.route("/no", methods=["POST"])
+# def optionno():
+    
 
 # # Used to test locally
 if __name__ == "__main__":
